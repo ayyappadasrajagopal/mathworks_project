790796ceab3f58f8ae46b0aa9e5dcee3a1f1f8f1
Ayyappadas R <ayyappadas.r.nair@gmail.com>
2025-08-12 10:40:42 +0530

    test_cases_removed

--- 9) Mathworks Project/main_code_tx.m
+++ 9) Mathworks Project/main_code_tx.m
@@ -1,30 +1,5 @@
-
 % Matlab Code for Wireless Transmission of Data
 % Dr. Ayyappadas Rajagopal
-
-
-% Test script for fractionalToBinary function
-% Define test cases
-testCases = [
-    struct('input', 10.625, 'precision', 5, 'expected', '1010.10100'),
-    struct('input', 0.75, 'precision', 4, 'expected', '0.1100'),
-    struct('input', 3.5, 'precision', 3, 'expected', '11.1'),
-    struct('input', 0.1, 'precision', 10, 'expected', '0.0001100110')
-];
-
-% Run tests
-for i = 1:length(testCases)
-    testCase = testCases(i);
-    result = fractionalToBinary(testCase.input, testCase.precision);
-
-    % Check if the result matches the expected output
-    if strcmp(result, testCase.expected)
-        fprintf('Test %d passed: %s -> %s\n', i, num2str(testCase.input), result);
-    else
-        fprintf('Test %d failed: %s -> %s (expected: %s)\n', ...
-                i, num2str(testCase.input), result, testCase.expected);
-    end
-end
 
 function binaryStr = fractionalToBinary(fraction, precision)
     % Validate input
